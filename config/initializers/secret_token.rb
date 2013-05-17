d4e03ab7a7b01fc49ce28c1d31063984bfba16f4
Haizyfox <bradbarrow@gmail.com>
2013-05-17 12:32:44 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trofy::Application.config.secret_token = 'e7832d1a97d8eae9eefcc9237a4b7e8173e8db013b69b82f43db2cbeef9264af81f963f67558d45f85a1a1748b15503ecbaec04eaf70fa8be2544935729fbec3'
